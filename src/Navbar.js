8319504b7b760b809242c1b794e065624b77a5a9
SuryatejaDuvvuri <duvvurisuryateja95@gmail.com>
2023-02-20 17:48:17 -0800

    Created blog cards

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,25 +2,26 @@
 import { WiDaySunny } from "react-icons/wi";
 const bar = () => {
   return (
-    <nav className="inline-block py-5 px-5 flex flex-row items-center justify-between flex-wrap mx-auto ">
-      <div className="w-full block lg:w-auto text-3xl tracking-wider">
-        <a href="#" class="mr-8">
+    <nav className="inline-block py-5 px-5 flex flex-row items-center justify-between flex-wrap space-x-4 ">
+      <div className="w-full block lg:w-auto text-3xl tracking-wider ">
+        <a href="#" class="mr-3 p-3 hover:border border-double rounded-lg hover:text-green-400">
           Home
         </a>
-        <a href="#" class="mr-8">
+        <a href="#" class="mr-3 p-3 hover:border border-double rounded-lg hover:text-green-400">
           About
         </a>
-        <a href="#" class="mr-8">
+        <a href="#" class="mr-3 p-3 hover:border border-double rounded-lg hover:text-green-400">
           Articles
         </a>
       </div>
-
-      <div class= "flex flex-row mt-4 lg:mt-0 mr-0">
+       <div class= "flex flex-row mt-4 lg:mt-0 mr-0">
           <div class="block px-4 py-2 text-lg border rounded border-white hover:border-transparent hover:bg-gradient-to-r from-red-500 to-yellow-500 mr-6 cursor-pointer">
             Login
           </div>
-          <WiDaySunny size = "45"/>
+          <WiDaySunny size = "45" className = "cursor-pointer"/>
       </div>
+
+
   
     </nav>
   );
