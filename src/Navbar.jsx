ee87931c606b35d2d7e3d1578a1608a06ac3cfa8
SuryatejaDuvvuri <duvvurisuryateja95@gmail.com>
2023-04-15 09:34:33 -0700

    Added state values to change the colors

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,36 +1,46 @@
+import { useState } from 'react';
 import { WiDaySunny } from "react-icons/wi";
 import Login from "./Login";
 
-export default function Bar() {
+export default function Bar(props) {
+
+  const {dark, isDark} = props;
+  // function handleClick()
+  // {
+  //   isDark(!dark);
+  // }
+
   return (
+    <>
     <nav className="mt-3 dark:text-white text-black py-5 px-5 flex flex-row items-center justify-between flex-wrap space-x-4 ">
       <div className="w-full block lg:w-auto text-3xl tracking-wider">
         <a
           href="https://suryatejaduvvuri.github.io/"
-          className="mr-3 p-3 rounded-lg text-black hover:text-4xl duration-200 dark:(hover:text-green-400)"
+          className={dark ? `mr-3 p-3 rounded-lg text-white hover:text-4xl hover:text-green-400 duration-200`: `mr-3 p-3 rounded-lg text-black hover:text-4xl duration-200`}
         >
           Home
         </a>
         <a
           href="https://suryatejaduvvuri.github.io/About.html"
-          className="mr-3 p-3 rounded-lg text-black hover:text-4xl duration-200 dark:(hover:text-green-400)"
+          className={dark ? `mr-3 p-3 rounded-lg text-white hover:text-4xl hover:text-green-400 duration-200` :`mr-3 p-3 rounded-lg text-black hover:text-4xl duration-200`}
         >
           About
         </a>
         <a
           href="#"
-          className="mr-3 p-3 rounded-lg text-black hover:text-4xl duration-200 dark:(hover:text-green-400)"
+          className={dark ? `mr-3 p-3 rounded-lg text-white hover:text-4xl hover:text-green-400 duration-200` : `mr-3 p-3 rounded-lg text-black hover:text-4xl hover:text-green-400 duration-200`}
         >
           Articles
         </a>
       </div>
       <div class="flex flex-row mt-4 lg:mt-0 mr-0">
         <Login />
-        <WiDaySunny
+        <WiDaySunny onClick = {() => isDark(!dark)}
           size="45"
-          className="cursor-pointer hover:scale-125 duration-200 text-black"
+          className={dark ? `cursor-pointer hover:scale-125 duration-200 text-white`:`cursor-pointer hover:scale-125 duration-200 text-black` }
         />
       </div>
     </nav>
+    </>
   );
 }
